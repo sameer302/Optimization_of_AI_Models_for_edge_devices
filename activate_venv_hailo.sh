cec8ebc1ffccb0bc8d44a44e03ff6b9a65fbd8cd
sameer <sameernilkhan11@gmail.com>
2026-05-21 16:04:40 +0530

    corrected some file paths after folder renaming

--- activate_venv_hailo.sh
+++ activate_venv_hailo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/sameer/Desktop/optimization_of_ai_models/hailo-apps
+cd /home/sameer/Desktop/Edge_AI/edge_ai_bottleneck_analysis/hailo-apps
 source setup_env.sh
 cd "$OLDPWD"
--- activate_venv_ultralytics.sh
+++ activate_venv_ultralytics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /home/sameer/Desktop/optimization_of_ai_models/ejtech_ultralytics/venv2/bin/activate
+source /home/sameer/Desktop/Edge_AI/edge_ai_bottleneck_analysis/ejtech_ultralytics/cpu_venv/bin/activate
 
 # To use this script, run:
 #   source activate_venv_ultralytics.sh
